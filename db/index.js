7af4c83d1bb9572063b9db2d21589cfaf8946ffc
gbroadhead <gwbroadhead@gmail.com>
2022-01-17 20:13:28 -0800

    updates

--- db/index.js
+++ db/index.js
@@ -1,3 +1,4 @@
 const { Sequelize } = require('sequelize')
+require('dotenv').config()
 
 module.exports = new Sequelize(process.env.JAWSDB_URL || 'mysql://root:rootroot@localhost:3306/techBlog_db')
